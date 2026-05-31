0b076c249c880869764ffaea27047a339e3ec111
Rich Fallat <richfallatjrshop@gmail.com>
2026-05-31 15:57:41 -0700

    Clean setup

---
+++ Gravitas Plague/Gravitas Plague/Gravitas_PlagueApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gravitas_PlagueApp.swift
+//  Gravitas Plague
+//
+//  Created by Richard Fallat on 5/31/26.
+//
+
+import SwiftUI
+
+@main
+struct Gravitas_PlagueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Gravitas Plague/Gravitas PlagueTests/Gravitas_PlagueTests.swift
@@ -0,0 +1,17 @@
+//
+//  Gravitas_PlagueTests.swift
+//  Gravitas PlagueTests
+//
+//  Created by Richard Fallat on 5/31/26.
+//
+
+import Testing
+@testable import Gravitas_Plague
+
+struct Gravitas_PlagueTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
